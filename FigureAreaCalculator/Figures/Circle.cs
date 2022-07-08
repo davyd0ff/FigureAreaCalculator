8a53d0aac920bac8538118574eb0a47f1e9aa4b0
Давыдов Аркадий Александрович <davydovaa43.1@amgpgu.ru>
2022-07-08 16:37:26 +1000

    Implementation of Circle

---
+++ FigureAreaCalculator/Figures/Circle.cs
@@ -0,0 +1,28 @@
+﻿using FigureAreaCalculator.Interfaces;
+
+namespace FigureAreaCalculator.Figures;
+
+internal class Circle : IFigure
+{
+  private readonly double _radius;
+
+  public Circle(double radius)
+  {
+    if (this.IsNotValidate(radius))
+    {
+      throw new ArgumentException("radius must be greater zero");
+    }
+
+    this._radius = radius;
+  }
+
+  private bool IsNotValidate(double radius)
+  {
+    return radius <= 0;
+  }
+
+  public double GetArea()
+  {
+    return Math.PI * this._radius * this._radius;
+  }
+}
