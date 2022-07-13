0f82bf093e7ab173ff44009ead97c792fca1ef4f
Ark Davydoff <30714009+davyd0ff@users.noreply.github.com>
2022-07-13 07:22:34 +0300

    Update SelectProducts.sql
    remove tab

--- task#2/SelectProducts.sql
+++ task#2/SelectProducts.sql
@@ -5,4 +5,4 @@ Select p.Name as ProductName, c.Name as CategoryName
 From 
   Products as p 
     Left join Intermediate_Product_Category as i on p.Id = i.ProductId
-	Left join Categories as c on i.CategoryId = c.Id
+    Left join Categories as c on i.CategoryId = c.Id
