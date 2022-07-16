045666a51b9be672d2abb584b860e4588a9c79fd
Ark Davydoff <ark.davyd0ff@gmail.com>
2022-07-16 22:22:49 +1000

    add validation to check if a triangle can exist

--- FigureAreaCalculator/Figures/Triangle.cs
+++ FigureAreaCalculator/Figures/Triangle.cs
@@ -2,30 +2,32 @@
 
 namespace FigureAreaCalculator.Figures;
 
-public class Triangle : IFigure, ITriangle
+public class Triangle : IFigure
 {
-  private readonly double _a;
-  private readonly double _b;
-  private readonly double _c;
   private double[] _sides;
 
   public Triangle(double a, double b, double c)
   {
-    if (this.IsNotValidate(a, b, c))
+    this._sides = new[] { a, b, c };
+    Array.Sort(this._sides);
+
+    if (this.SidesNotGraterZero())
     {
       throw new ArgumentException("sides of triangle must be greater zero");
     }
-
-    this._a = a;
-    this._b = b;
-    this._c = c;
-
-    this._sides = new[] { a, b, c };
+    if (this.TriangleCanNotBeExist())
+    {
+      throw new ArgumentException("triangle cannot be exists, because third side greater sum of other");
+    }
   }
 
-  private bool IsNotValidate(double a, double b, double c)
+  private bool SidesNotGraterZero()
   {
-    return a <= 0 || b <= 0 || c <= 0;
+    return this._sides[0] <= 0;
+  }
+  private bool TriangleCanNotBeExist()
+  {
+    return this._sides[0] + this._sides[1] <= this._sides[2];
   }
 
   public double GetArea()
@@ -38,12 +40,9 @@ public class Triangle : IFigure, ITriangle
 
   public bool IsRight()
   {
-    var sortedSides = this._sides;
-    Array.Sort(sortedSides);
-
-    var cathetus1 = sortedSides[0];
-    var cathetus2 = sortedSides[1];
-    var hypotenuse = sortedSides[2];
+    var cathetus1 = this._sides[0];
+    var cathetus2 = this._sides[1];
+    var hypotenuse = this._sides[2];
 
     return hypotenuse * hypotenuse == cathetus1 * cathetus1 + cathetus2 * cathetus2;
   }
