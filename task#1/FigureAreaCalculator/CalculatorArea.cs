e5a31b9e60673bd7658e3361c946cf86b06c539f
Ark Davydoff <ark.davyd0ff@gmail.com>
2022-07-16 23:14:57 +1000

    Add static class to calculate area

---
+++ task#1/FigureAreaCalculator/CalculatorArea.cs
@@ -0,0 +1,11 @@
+﻿using FigureAreaCalculator.Interfaces;
+
+namespace FigureAreaCalculator;
+
+public static class CalculatorArea
+{
+  public static double Calculate<T>(T figure) where T : IFigure
+  {
+    return figure.GetArea();
+  }
+}
