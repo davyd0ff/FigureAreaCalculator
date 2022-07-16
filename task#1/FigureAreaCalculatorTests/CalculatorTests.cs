ca7b0dbfcc67dc4aae0d155e12b9b5e8ed84e13b
Ark Davydoff <ark.davyd0ff@gmail.com>
2022-07-16 23:16:10 +1000

    add tests for CalculatorArea

---
+++ task#1/FigureAreaCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using FigureAreaCalculator;
+using FigureAreaCalculator.Interfaces;
+using Moq;
+
+namespace FigureAreaCalculatorTests;
+
+internal class CalculatorTests
+{
+  [Test]
+  public void Calculate()
+  {
+    var mockedFigure = new Mock<IFigure>();
+
+    CalculatorArea.Calculate(mockedFigure.Object);
+
+    mockedFigure.Verify(m => m.GetArea(), Times.Once);
+  }
+}
